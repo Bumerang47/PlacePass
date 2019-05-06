e471b98381697054bb20dbcfcf0a7669c7feaec5
Alex-Ul <it.bumerang@gmail.com>
2019-05-06 16:51:58 +0300

    update facebook oauth params and add compare users by email

--- backPlacePass/PlacePass/settings.py
+++ backPlacePass/PlacePass/settings.py
@@ -188,5 +188,17 @@ SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('AUTH_FACEBOOK_SECRET', '')
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
   'locale': 'ru_RU',
-  'fields': 'id, name, email, age_range'
+  'fields': 'id, name, email'
 }
+SOCIAL_AUTH_FACEBOOK_API_VERSION = '3.3'
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.user.user_details',
+)
